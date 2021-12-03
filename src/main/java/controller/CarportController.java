cdc01488b4477cbd692d5cd8e84c394b233821a7
Nikolaj <nikolajweinell@hotmail.com>
2021-12-03 11:32:21 +0100

    carportcontroll can calc pillar and rem

--- src/main/java/controller/CarportController.java
+++ src/main/java/controller/CarportController.java
@@ -1,4 +1,33 @@
 package controller;
 
 public class CarportController {
+
+    int length, width;
+
+    public CarportController(int length, int width) {
+
+        this.length = length;
+        this.width = width;
+    }
+
+    public int returnCarportPillarLightRoofNoShed() {
+        // Stolpe hvert 3m
+        int pillarSpace = 300;
+        int amountPillars = (int) (Math.ceil(length/pillarSpace) + 1) * 2;
+        if (amountPillars <= 4) amountPillars = 4;
+
+        return amountPillars;
+    }
+
+    public int returnCarportRemLightRoofNoShed() {
+        // 55cm afstand mellem rembrædder
+        int remSpace = 55;
+        int amountRem = length/remSpace;
+
+        return amountRem;
+    }
+
+    
+
+
 }
