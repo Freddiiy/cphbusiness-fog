7bcbbdf4298c66271f065b7ba72bbb8ceda35579
Nikolaj <nikolajweinell@hotmail.com>
2021-12-05 22:57:28 +0100

    Add more calc functionalities

--- src/main/java/controller/CarportController.java
+++ src/main/java/controller/CarportController.java
@@ -11,7 +11,6 @@ public class CarportController {
     }
 
     public int returnCarportPillarLightRoofNoShed() {
-        // Stolpe hvert 3m
         int pillarSpace = 300;
         int amountPillars = (int) (Math.ceil(length/pillarSpace) + 1) * 2;
         if (amountPillars <= 4) amountPillars = 4;
@@ -19,15 +18,62 @@ public class CarportController {
         return amountPillars;
     }
 
-    public int returnCarportRemLightRoofNoShed() {
-        // 55cm afstand mellem rembrædder
+    public int returnCarportRaftersLightRoofNoShed() {
         int remSpace = 55;
-        int amountRem = length/remSpace;
+        int amountRafters = length/remSpace;
 
-        return amountRem;
+        return amountRafters;
     }
 
-    
+    public void calcMaterials(int amountPillars, int amountRafters)    {
+
+        int packOfScrewsBund = 200;
+        int holetape = 1000;
+        int holetapeRolls = 0;
+        int universalR = 0;
+        int universalL = 0;
+        int packOfScrewsStern = 200;
+        int screwsUniversals = 0;
+        int packofScrewsUnivsersal = 250;
+        int bolt = 0;
+        int squarePiece = 0;
+        int screw70mm = 400;
+        int screw50mm = 300;
 
 
+        // For cirumference buy this many holetape rolls
+        holetapeRolls = (length*2 + width+2)/holetape;
+
+        // For every pillar use these materials
+        for (int i = 0; i < amountPillars; i++) {
+            //
+            bolt++;
+            bolt++;
+            squarePiece++;
+        }
+
+        // For every rafter use these materials
+        for (int i = 0; i < amountRafters; i++) {
+            // Universals + their screws
+            universalR++;
+            universalL++;
+            screwsUniversals = universalL*3 + universalR*3;
+
+            // Holetape screws
+            screwsUniversals += amountRafters*4;
+        }
+
+        // Buy this many packs of screws
+        packofScrewsUnivsersal = packofScrewsUnivsersal/screwsUniversals;
+
+
+        System.out.println("Amount of holetape rolls: " + holetapeRolls);
+        System.out.println("Amount of bolts: " + bolt);
+        System.out.println("Amount of square pieces: " + squarePiece);
+        System.out.println("Amount of univerals R: " + universalR);
+        System.out.println("Amount of univerals L: " + universalL);
+        System.out.println("Amount of screws: " + screwsUniversals);
+        System.out.println("Amount of pack of screws to buy: " + packofScrewsUnivsersal);
+
+    }
 }
