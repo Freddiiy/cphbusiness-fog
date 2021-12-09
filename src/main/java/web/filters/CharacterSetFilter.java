5a0c482fd12a759b5dc360dc07b2bcb85ddf45fb
Freddiiy <frederikgaller@live.dk>
2021-12-09 12:59:33 +0100

    fixed filter :) headache

--- src/main/java/web/filters/CharacterSetFilter.java
+++ src/main/java/web/filters/CharacterSetFilter.java
@@ -7,7 +7,7 @@ public class CharacterSetFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
+        response.setContentType("text/css; charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
         filterChain.doFilter(request, response);
     }
